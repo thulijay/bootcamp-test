beb1b0f85b080b47c96a153c39a73efec1c25c22
thulijay <thulijay79@gmail.com>
2020-04-17 04:27:04 +0200

    changes

--- test/countRegNumber.test.js
+++ test/countRegNumber.test.js
@@ -1,6 +1,9 @@
 describe('Returns the number of registration numbers in a string', function(){
 
-    it('Should return 3 for the total number of registartion numbers there is', function(){
+    it('Should return 3 for the total number of registartion numbers', function(){
         assert.deepEqual(countRegNumber('CA 182736,CY 523519,CJ 812328'), 3);
-    })
-})
+    });
+    it('Should return 4 for the total number of registration numbers', function(){
+        assert.deepEqual(countRegNumber('CY 254896,CAA 458976,CK 579458,CJ 876319'), 4);
+    });
+});
--- test/mostProfitableDepartment.test.js
+++ test/mostProfitableDepartment.test.js
@@ -18,6 +18,27 @@ describe('Calculates which department is the most profitable', function(){
         {"department":"electronics","sales":16109,"day":"Saturday"},{"department":"steelwork","sales":7500,"day":"Tuesday"},
         {"department":"outdoor","sales":2505,"day":"Tuesday"},{"department":"carpentry","sales":1540,"day":"Tuesday"},
         {"department":"steelwork","sales":1500,"day":"Wednesday"},{"department":"carpentry","sales":8009,"day":"Wednesday"}];
-        assert.deepEqual(mostProfitableDepartment(department), 'carpentry')
+        assert.deepEqual(mostProfitableDepartment(department), 'carpentry');
     });
+    it('Should return "hardware" as the most profitable department', function(){
+        var department = [{"department":"hardware","sales":4500,"day":"Monday"},{"department":"outdoor","sales":1500,"day":"Monday"},
+        {"department":"carpentry","sales":5500,"day":"Monday"},{"department":"carpentry","sales":7500,"day":"Tuesday"},
+        {"department":"outdoor","sales":2505,"day":"Tuesday"},{"department":"hardware","sales":1540,"day":"Tuesday"},
+        {"department":"carpentry","sales":1500,"day":"Wednesday"},{"department":"outdoor","sales":8507,"day":"Wednesday"},
+        {"department":"carpentry","sales":8009,"day":"Wednesday"},{"department":"hardware","sales":12000,"day":"Thursday"},
+        {"department":"outdoor","sales":18007,"day":"Thursday"},{"department":"hardware","sales":6109,"day":"Thursday"},
+        {"department":"carpentry","sales":7005,"day":"Friday"},{"department":"outdoor","sales":12006,"day":"Friday"},
+        {"department":"hardware","sales":16109,"day":"Friday"},{"department":"electronics","sales":4500,"day":"Monday"},
+        {"department":"outdoor","sales":1500,"day":"Monday"},{"department":"hardware","sales":5500,"day":"Monday"},
+        {"department":"steelwork","sales":7500,"day":"Tuesday"},{"department":"outdoor","sales":2505,"day":"Tuesday"},
+        {"department":"hardware","sales":1540,"day":"Tuesday"},{"department":"carpentry","sales":1500,"day":"Wednesday"},
+        {"department":"outdoor","sales":8507,"day":"Wednesday"},{"department":"hardware","sales":8009,"day":"Wednesday"},
+        {"department":"carpentry","sales":12000,"day":"Thursday"},{"department":"hardware","sales":6109,"day":"Thursday"},
+        {"department":"carpentry","sales":7005,"day":"Friday"},{"department":"electronics","sales":12006,"day":"Friday"},
+        {"department":"electronics","sales":16109,"day":"Saturday"},{"department":"steelwork","sales":7500,"day":"Tuesday"},
+        {"department":"outdoor","sales":2505,"day":"Tuesday"},{"department":"hardware","sales":1540,"day":"Tuesday"},
+        {"department":"steelwork","sales":1500,"day":"Wednesday"},{"department":"hardware","sales":8009,"day":"Wednesday"}];
+        assert.deepEqual(mostProfitableDepartment(department), 'hardware');
+    });
+
 });
